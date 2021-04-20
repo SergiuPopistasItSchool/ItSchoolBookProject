a8e5fe2f1ad6197470253cf8d50dce1ab4c6ae7e
SergiuPopistasItSchool <sergiu.popistas@itschool.ro>
2021-04-20 18:27:17 +0300

    added mode in csv reader

--- ItSchoolBookApp.py
+++ ItSchoolBookApp.py
@@ -3,7 +3,7 @@ def add_book():
     author_name = input("Insert a author name ->")
     #imporint csv lib
     import csv
-    with open('booksDB.csv', 'w') as file:
+    with open('booksDB.csv', mode='w') as file:
         writer = csv.DictWriter(file,fieldnames=[
             "BookName", "AuthorName", "SharedWith", "IsRead"
         ])
